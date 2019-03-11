d873ac611aa27d5ad690b31112f2012f5f4ffca5
Carlos Moisés Gil Solanas <carlos.gil@MADRID.ALTIA.LOCAL>
2019-03-11 16:49:52 +0100

    cabecera arreglada

--- Practica 1/Cosas Varias/cabecera.php
+++ Practica 1/Cosas Varias/cabecera.php
@@ -1,5 +1,8 @@
 <div id="cabecera">
-
-		<h1><img src="icono.jpg" width="30" height="30">ASTEYO</h1>
+		
+		<h1>
+		<img src="icono.jpg" width="90" height="90" id="icono"> 
+		ASTEYO</h1>
+		
 
 </div>
